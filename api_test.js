bdde1b6a5d43277a2d533758969766288c6b0e95
Andrew Naguib <andrewnaguibaziz@yahoo.com>
2021-03-18 22:34:16 -0700

    log screen, background, icon

--- api_test.js
+++ api_test.js
@@ -1,5 +1,5 @@
 
-
+/*
 //          https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
 
 
@@ -24,3 +24,34 @@ request.onload = function ()
 }
 
 request.send()
+
+*/
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+const url = 'https://randomuser.me/api/?results=10';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
